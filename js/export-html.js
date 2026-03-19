65211746a0252709dcb0e08971bfdf6769c534e9
sherlockes <sherlockes@yahoo.es>
2026-03-19 13:16:55 +0100

    Update

--- js/export-html.js
+++ js/export-html.js
@@ -7,7 +7,7 @@ function comprimirBase64(base64Str, calidad = 0.7) { // Subimos calidad a 0.85
         img.onload = () => {
             const canvas = document.createElement('canvas');
             const ctx = canvas.getContext('2d');
-            const MAX_WIDTH = 1200; // Antes 800, ahora 1600 para alta definición
+            const MAX_WIDTH = 800; // Antes 800, ahora 1600 para alta definición
             let width = img.width;
             let height = img.height;
             
@@ -241,7 +241,7 @@ async function generarHTML(desviaciones, seccionId) {
                     img.onload = () => {
                         const canvas = document.createElement('canvas');
                         const ctx = canvas.getContext('2d');
-                        const MAX_WIDTH = 1200; // Calidad de sobra para la columna de subsanación
+                        const MAX_WIDTH = 800; // Calidad de sobra para la columna de subsanación
                         canvas.width = MAX_WIDTH; 
                         canvas.height = (MAX_WIDTH * img.height) / img.width;
                         ctx.fillStyle = "#FFFFFF"; ctx.fillRect(0,0,canvas.width,canvas.height);
